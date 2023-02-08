1557e3000e427299e311ecda57b880ca18c3b22c
auberr <auberr@daum.net>
2023-02-08 19:29:55 +0900

    test: article, users

--- users/tests.py
+++ users/tests.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from users.models import User
 
 
-class UserRegistrationAPIViewTestCase(APITestCase):
+class UserCreateViewTestCase(APITestCase):
     def test_registration(self):
         url = reverse("user_create_view")
         user_data = {
@@ -12,22 +12,33 @@ class UserRegistrationAPIViewTestCase(APITestCase):
             "password" : "password1!",
             "password2" : "password1!",
         }
+
         response = self.client.post(url, user_data)
         self.assertEqual(response.status_code, 201)
 
 
-class LoginUserTest(APITestCase):
-    def setUp(self):
-        self.data = {'username': 'testuser', 'password':'password1!'}
-        self.user = User.objects.create_user('testuser', 'password1!')
+class UserAuthViewTestCase(APITestCase):
+    # self.data = {'username': 'testuser', 'password':'password1!'}
+    # self.user = User.objects.create_user('testuser', 'password1!')
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data = {'username':'john', 'password':'password1!'}
+        cls.user = User.objects.create_user('john', 'password1!')
+
+        # for user_id in range(113):
+        #     User.objects.create_user(
+        #         username=f'john{user_id}', password=f'password1!{user_id}'
+        #     )
+        
+    
         
     def test_login(self):
         url = reverse("user_auth_view")
-        response = self.client.post(url, self.data)
+        response = self.client.post(url, self.user_data)
         self.assertEqual(response.status_code, 200)
             
     def test_get_user_data(self):
-        access_token = self.client.post(reverse('user_auth_view'), self.data).data['access']
+        access_token = self.client.post(reverse('user_auth_view'), self.user_data).data['access']
         response = self.client.get(path=reverse("user_auth_view"), HTTP_AUTHORIZATION=f"Bearer {access_token}")
         print(response.data)
         self.assertEqual(response.status_code, 200)
